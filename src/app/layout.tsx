511f88b5854b71e8d29ecdc51ab43ea3c313ca44
Leonardo Figueiredo <leodbf@hotmail.com>
2024-01-31 15:36:34 -0300

    fix: adicionando meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        
+      <meta name="MobileOptimized" content="320" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <Header />
